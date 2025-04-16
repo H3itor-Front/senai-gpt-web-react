d4b67f38535b8c06f0ff845c3b2a485f3defc58e
FIC <fic@info.local>
2025-04-15 21:37:52 -0300

    atualizando pasta

--- vite-project/src/pages/login/index.jsx
+++ vite-project/src/pages/login/index.jsx
@@ -3,13 +3,46 @@ import "./login.css";
 
 function Login() {
 
-    return (
-      <>
-      
-       <h1>Login</h1>
-       
-      </>
-    )
-  }
-  
-  export default Login;
+  return (
+    <>
+      <header></header>
+
+      <Main className="page-container">
+
+
+        <div className="robo-image">
+
+        </div>
+
+        <div className="login-container">
+
+          <img className="Logo" src="../assets/img/BLA.png" alt="Logo do SenaiGPT." />
+
+          <h1 
+            id="meutitulo"
+            className="titulo"
+          >Login</h1>
+
+          <input className="input" type="email"
+            placeholder="Insira o email" />
+          <input className="input" type="password"
+            placeholder="Insira sua senha" />
+
+          <button className="btn">Entrar</button>
+
+
+
+
+        </div>
+
+
+
+      </Main>
+
+      <footer></footer>
+
+    </>
+  )
+}
+
+export default Login;
