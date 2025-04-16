00fff3011930a16be51c3b776d5625ad29202794
FIC <fic@info.local>
2025-04-16 20:31:12 -0300

    atualizando lição

--- vite-project/src/pages/login/index.jsx
+++ vite-project/src/pages/login/index.jsx
@@ -1,13 +1,25 @@
 import "./login.css";
 
+import logo from "../../assets/img/BLA.png";
+
+import { useState } from "react";
 
 function Login() {
+
+  const [email, setEmail] = useState ();
+  const [password, setPassword] = useState();
+
+  const onLoginClick = () => {
+
+alert("funcionou!")
+
+  }
 
   return (
     <>
       <header></header>
 
-      <Main className="page-container">
+    <main className="page-container">
 
 
         <div className="robo-image">
@@ -16,19 +28,19 @@ function Login() {
 
         <div className="login-container">
 
-          <img className="Logo" src="../assets/img/BLA.png" alt="Logo do SenaiGPT." />
+          <img className="Logo" src={logo} alt="Logo do SenaiGPT." />
 
-          <h1 
+          <h1
             id="meutitulo"
             className="titulo"
           >Login</h1>
 
-          <input className="input" type="email"
-            placeholder="Insira o email" />
-          <input className="input" type="password"
-            placeholder="Insira sua senha" />
 
-          <button className="btn">Entrar</button>
+
+          <input className="input"value={email} onChange={Event => setEmail(Event.target.value)} type="email"placeholder="Insira o email" />
+          <input className="input"value={password} onChange={Event => setPassword(Event.target.value)} type="password" placeholder="Insira sua senha" />
+
+          <button class="btn" onClick={() => onLoginClick()}>Entrar</button>
 
 
 
@@ -37,7 +49,7 @@ function Login() {
 
 
 
-      </Main>
+      </main>
 
       <footer></footer>
 
