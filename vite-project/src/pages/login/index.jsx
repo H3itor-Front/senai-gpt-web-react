0e39562d8708f864920e0062f8742ca77ba9cf7d
FIC <fic@info.local>
2025-04-17 21:20:59 -0300

    finalizando a tela de login

--- vite-project/src/pages/login/index.jsx
+++ vite-project/src/pages/login/index.jsx
@@ -4,35 +4,70 @@ import { useState } from "react";
 
 function Login() {
 
-  const [email, setEmail] = useState ();
+  const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
   const onLoginClick = async () => {
 
-let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login",{
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
 
-  headers:{
+      headers: {
 
         "content-type": "application/json"
 
-  },
-  method: "POST",
-  body:JSON.stringify({
-      email: email, 
-      password: password
-  })
+      },
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
 
-});
+    });
 
-console.log(response);  
+    console.log(response);
 
+    if (response.ok == true) {
+
+      alert("Login realizado")
+
+      let json = await response.json();
+
+      let token = json.accesToken;
+
+      console.log ("token" + token);
+
+      localStorage.setItem("Meu token", token);
+
+      // function setCookie(name, value, days) {
+
+      //   const date = new Date();
+      //   date.setTime(date.getTime() + (days * 24  * 60 * 60 * 1000)); // dias -> ms
+      //   const expires = "expires=" + date.toUTCString();
+      //   DocumentTimeline.cookie = '${name}=${value}, ${expires};  path/';        
+      // }
+      // setCookie("meuToken", token, 7);
+
+      window.location.href = "/chat";
+
+    } else {
+
+      if (response.status == 401) {
+
+        alert("Credenciais incorretas. Tente novamente")
+
+      } else {
+
+        alert("Erro inesperado aconteceu, caso persista, contate os administradores.")
+
+      }
+    }
   }
 
   return (
     <>
       <header></header>
 
-    <main className="page-container">
+      <main className="page-container">
 
 
         <div className="robo-image">
@@ -50,8 +85,8 @@ console.log(response);
 
 
 
-          <input className="input"value={email} onChange={Event => setEmail(Event.target.value)} type="email"placeholder="Insira o email" />
-          <input className="input"value={password} onChange={Event => setPassword(Event.target.value)} type="password" placeholder="Insira sua senha" />
+          <input className="input" value={email} onChange={Event => setEmail(Event.target.value)} type="email" placeholder="Insira o email" />
+          <input className="input" value={password} onChange={Event => setPassword(Event.target.value)} type="password" placeholder="Insira sua senha" />
 
           <button class="btn" onClick={() => onLoginClick()}>Entrar</button>
 
